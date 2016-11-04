43e3c5177b8ef951884091e26e0f18ae937940c0
Steven Varoumas <steven.varoumas@gmail.com>
2016-11-04 13:54:55 +0100

    fix bug in whyml printing

--- tests/foo.ml
+++ tests/foo.ml
@@ -1,6 +1,7 @@
 
 
 let%node test ~i:(x) ~o:(z) =
-  pre (x >= 0); post (z >= x); inv (z = x+y && y >=0);
-  y = 0 ->> (y + 1);
-  z = x + y
+  pre (x >= 0); post (z >= x); inv ( a >= 0 && b >= 0 );
+  a = 0 ->> (a + 1);
+  b = x ->> (b + 1); 
+  z = x + a + b 
