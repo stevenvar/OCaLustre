d757af3e017cdca6e0a130565f6c3e698115933a
stevenvar <steven.varoumas@gmail.com>
2016-11-04 14:04:39 +0100

    new test

--- tests/foo.ml
+++ tests/foo.ml
@@ -1,6 +1,7 @@
     
 
 let%node test ~i:(x) ~o:(z) =
-  pre (x >= 0); post (z >= x); inv (y >= 0);
+  pre (x >= 0); post (z >= x); inv (y >= 0 && w >= b);
   y = 0 ->> (y + 1);
+  w = x ->> (w + 1);
   z = x + y
