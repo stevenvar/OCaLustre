376a5bfb7a6173dbbc74b84b1d17f7e3e7cf904d
Steven Varoumas <steven.varoumas@gmail.com>
2018-11-12 15:58:03 +0100

    normalization

--- src/normalize2.ml
+++ src/normalize2.ml
@@ -5,26 +5,56 @@ open Error
 
 (** Returns a new equation and the corresponding variable **)
 let new_eq x e =
-  let name = x^"_aux" in
+  let name = x in
   let pat = { p_desc = Ident name ; p_loc = e.e_loc} in
   { pattern = pat  ; expression = e } ,
   { e_desc = Variable name ; e_loc = e.e_loc }
 
+let cpt = ref 0
+let fresh x =
+  incr cpt;
+  "_"^x^"_aux"^(string_of_int !cpt)
+
 
 let rec norm_exp x exp =
   match exp.e_desc with
-  | Value k -> ([], exp)
-  | Variable v -> ([],exp)
   | InfixOp (op,e1,e2) ->
-    let (d1,e1') = norm_exp (x^"1") e1 in
-    let (d2,e2') = norm_exp (x^"2") e2 in
+    let (d1,e1') = norm_exp (fresh x) e1 in
+    let (d2,e2') = norm_exp (fresh x) e2 in
     (d1@d2, { exp with e_desc = InfixOp(op,e1',e2') })
   | Fby(k,e) ->
-     let (d,e') = norm_exp (x^"_aux") e in
+    let x = fresh x in
+     let (d,e') = norm_exp x e in
      let exp' = { exp with e_desc = Fby(k,e') } in
      let eq',var' = new_eq x exp' in
      (eq'::d,var')
-  |_ -> failwith "not ready"
+  | Alternative(e1,e2,e3) ->
+    let (d1,e1') = norm_exp (fresh x) e1 in
+    let (d2,e2') = norm_exp (fresh x) e2 in
+    let (d3,e3') = norm_exp (fresh x) e3 in
+    (d1@d2@d3, { exp with e_desc = Alternative(e1',e2',e3') })
+  | PrefixOp(op,e) ->
+    let (d,e') = norm_exp x e in
+    (d, { exp with e_desc = PrefixOp(op,e') })
+  | When(e1,e2) ->
+    let (d,e') = norm_exp x e1 in
+    (d, { exp with e_desc = When(e',e2) })
+  | Whennot(e1,e2) ->
+    let (d,e') = norm_exp x e1 in
+    (d, { exp with e_desc = Whennot(e',e2) })
+  | Merge(e1,e2,e3) ->
+    let (d1,e1') = norm_exp (fresh x) e1 in
+    let (d2,e2') = norm_exp (fresh x) e2 in
+    let (d3,e3') = norm_exp (fresh x) e3 in
+    (d1@d2@d3, { exp with e_desc = Merge(e1',e2',e3') })
+  | Application (f,n,e) ->
+    let x = fresh x in
+    let (d,e') = norm_exp x e in
+    let exp' = { exp with e_desc = Application(f,n,e') } in
+    let eq',var' = new_eq x exp' in
+    (eq'::d,var')
+  | _ -> ([],exp)
+
 
 
 let rec norm_eqn eq =
@@ -32,7 +62,9 @@ let rec norm_eqn eq =
   |  Ident x -> begin
       match eq.expression.e_desc with
       | Fby (k,e) -> let (d,e') = norm_exp x (e) in
-                     (d,{eq with expression = {eq.expression with e_desc = Fby(k,e')}})
+        (d,{eq with expression = {eq.expression with e_desc = Fby(k,e')}})
+      | Application (f,n,e) -> let (d,e') = norm_exp x (e) in
+            (d,{eq with expression = {eq.expression with e_desc = Application(f,n,e')}})
       | e -> let (d,e') = norm_exp x eq.expression in
              (d,{eq with expression = e'})
     end
